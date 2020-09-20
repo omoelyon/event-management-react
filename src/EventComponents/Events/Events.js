f3463f85271cd56fc2a767ef921be121c436bfb3
omoelyon <ganiyuelegbede@yahoo.com>
2020-09-20 21:20:15 +0100

    restructured event

--- src/EventComponents/Events/Events.js
+++ src/EventComponents/Events/Events.js
@@ -2,17 +2,28 @@ import React from "react";
 
 import Event from "../Event/Event";
 import styles from "./Events.module.css";
+import { Card, ListGroup } from "react-bootstrap";
 
 const Events = (props) => {
   return (
     <div>
       <h1>these are my Events</h1>
       <div className={styles.Events}>
-        
         <Event />
         <Event />
-        <Event />
-        <Event />
+
+        <Card style={{ width: "80%", alignItems: "center" }}>
+          <Card.Header>Featured</Card.Header>
+          <ListGroup variant="flush">
+            {/* <ListGroup.Item>Cras justo odio</ListGroup.Item> */}
+            <ListGroup.Item>
+              <Event />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Event />
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </div>
     </div>
   );
